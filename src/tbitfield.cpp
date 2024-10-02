2395e068e7cd23cbbffc30ead69a80e2985e7e0a
Alexey Gutyansky <75169888+AlexixRugis@users.noreply.github.com>
2024-10-02 19:09:13 +0300

    bit operations repaired

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -7,6 +7,26 @@
 
 #include "tbitfield.h"
 
+#define USE_CACHED_OUTPUT
+
+// Returns a bitmask where N lower bits set
+#define FBITS(N) ((static_cast<TELEM>(1) << (N)) - static_cast<TELEM>(1))
+
+// Returns the number of bits of the index number that are needed to index all the bits of a value of type T
+template<typename T>
+constexpr int GetIndexBits()
+{
+    int n = 8 * sizeof(T);
+    int c = 0;
+    while ((n & 1) == 0)
+    {
+        ++c;
+        n >>= 1;
+    }
+
+    return c;
+}
+
 // Fake variables used as placeholders in tests
 static const int FAKE_INT = -1;
 static TBitField FAKE_BITFIELD(1);
@@ -39,13 +59,13 @@ TBitField::~TBitField()
 int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
 {
     if (n < 0 || n >= BitLen) throw range_error("TBitField::GetMemIndex : param n is out of bounds.");
-    return n / (8 * sizeof(TELEM));
+    return n >> GetIndexBits<TELEM>();
 }
 
 TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
 {
     if (n < 0 || n >= BitLen) throw range_error("TBitField::GetMemIndex : param n is out of bounds.");
-    return 1 << n % (8 * sizeof(TELEM));
+    return static_cast<TELEM>(1) << (n & FBITS(GetIndexBits<TELEM>()));
 }
 
 // доступ к битам битового поля
@@ -73,7 +93,7 @@ int TBitField::GetBit(const int n) const // получить значение б
 {
     if (n < 0 || n >= BitLen) throw range_error("TBitField::GetMemIndex : param n is out of bounds.");
 
-    return (pMem[GetMemIndex(n)] & GetMemMask(n)) != 0;
+    return (pMem[GetMemIndex(n)] & GetMemMask(n)) != static_cast<TELEM>(0);
 }
 
 // битовые операции
@@ -97,7 +117,7 @@ int TBitField::operator==(const TBitField &bf) const // сравнение
 {
     if (bf.GetLength() != GetLength()) return 0;
 
-    for (size_t i = 0; i < MemLen; i++)
+    for (int i = 0; i < MemLen; i++)
     {
         if (bf.pMem[i] != pMem[i])
         {
@@ -117,21 +137,17 @@ TBitField TBitField::operator|(const TBitField &bf) // операция "или"
 {
     TBitField bitfield(max(GetLength(), bf.GetLength()));
 
-    for (size_t i = 0; i < GetLength(); i++)
+    for (int i = 0; i < MemLen - 1; i++)
     {
-        if (GetBit(i))
-        {
-            bitfield.SetBit(i);
-        }
+        bitfield.pMem[i] |= pMem[i];
     }
+    bitfield.pMem[MemLen - 1] |= (pMem[MemLen - 1] & FBITS(BitLen & FBITS(GetIndexBits<TELEM>())));
 
-    for (size_t i = 0; i < bf.GetLength(); i++)
+    for (int i = 0; i < bf.MemLen - 1; i++)
     {
-        if (bf.GetBit(i))
-        {
-            bitfield.SetBit(i);
-        }
+        bitfield.pMem[i] |= bf.pMem[i];
     }
+    bitfield.pMem[bf.MemLen - 1] |= (bf.pMem[bf.MemLen - 1] & FBITS(bf.BitLen & FBITS(GetIndexBits<TELEM>())));
 
     return bitfield;
 }
@@ -140,13 +156,13 @@ TBitField TBitField::operator&(const TBitField &bf) // операция "и"
 {
     TBitField bitfield(max(GetLength(), bf.GetLength()));
 
-    for (size_t i = 0; i < min(GetLength(), bf.GetLength()); i++)
+    int minML = min(MemLen, bf.MemLen);
+    int minBL = min(BitLen, bf.BitLen);
+    for (int i = 0; i < minML - 1; i++)
     {
-        if (GetBit(i) && bf.GetBit(i))
-        {
-            bitfield.SetBit(i);
-        }
+        bitfield.pMem[i] = pMem[i] & bf.pMem[i];
     }
+    bitfield.pMem[minML - 1] = (pMem[minML - 1] & bf.pMem[minML - 1] & FBITS(minBL & FBITS(GetIndexBits<TELEM>())));
 
     return bitfield;
 }
@@ -155,13 +171,12 @@ TBitField TBitField::operator~(void) // отрицание
 {
     TBitField bitfield(GetLength());
 
-    for (size_t i = 0; i < GetLength(); i++)
+    for (int i = 0; i < MemLen - 1; i++)
     {
-        if (!GetBit(i))
-        {
-            bitfield.SetBit(i);
-        }
+        bitfield.pMem[i] = ~pMem[i];
     }
+    
+    bitfield.pMem[MemLen - 1] = ((~pMem[MemLen - 1]) & FBITS(BitLen & FBITS(GetIndexBits<TELEM>())));
 
     return bitfield;
 }
@@ -170,7 +185,7 @@ TBitField TBitField::operator~(void) // отрицание
 
 istream &operator>>(istream &istr, TBitField &bf) // ввод
 {
-    for (size_t i = 0; i < bf.GetLength(); i++)
+    for (int i = 0; i < bf.GetLength(); i++)
     {
         char bit;
         istr >> bit;
@@ -190,12 +205,20 @@ istream &operator>>(istream &istr, TBitField &bf) // ввод
 
 ostream &operator<<(ostream &ostr, const TBitField &bf) // вывод
 {
+#ifdef USE_CACHED_OUTPUT
     string s(bf.GetLength(), '0');
-    for (size_t i = 0; i < bf.GetLength(); i++)
+    for (int i = 0; i < bf.GetLength(); i++)
     {
         if (bf.GetBit(i)) s[i] = '1';
     }
 
     ostr << s;
+#else
+    for (int i = 0; i < bf.GetLength(); i++)
+    {
+        ostr << bf.GetBit(i);
+    }
+#endif // USE_CACHED_OUTPUT
+
     return ostr;
 }
